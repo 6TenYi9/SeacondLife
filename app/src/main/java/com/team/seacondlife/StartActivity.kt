d5dc68959176b466110ac8a8a031cf59ec1fbb43
Daniel Mora <daniel27mora96@gmail.com>
2023-01-17 12:36:06 +0100

    Added empty activities

--- app/src/main/java/com/team/seacondlife/StartActivity.kt
+++ app/src/main/java/com/team/seacondlife/StartActivity.kt
@@ -1,11 +1,37 @@
 package com.team.seacondlife
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        var textSignUp = findViewById<TextView>(R.id.textSignUp)
+        var buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
+        textSignUp.setOnClickListener(View.OnClickListener { openSignup() })
+
+        buttonLogin.setOnClickListener(View.OnClickListener { openLogin() })
+
+    }
+
+    fun openLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+
+    fun openSignup() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
     }
 }
