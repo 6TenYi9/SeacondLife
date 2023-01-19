ffb65bdca44b472442712b3589e1d2f1c712bbee
Daniel Mora <daniel27mora96@gmail.com>
2023-01-19 09:24:51 +0100

    Login button now leads to MainActivity

--- app/src/main/java/com/team/seacondlife/LoginActivity.kt
+++ app/src/main/java/com/team/seacondlife/LoginActivity.kt
@@ -1,11 +1,26 @@
 package com.team.seacondlife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener(View.OnClickListener { openMain() })
+
     }
+
+    fun openMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
 }
