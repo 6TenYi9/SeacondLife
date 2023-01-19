4b57f9016483a3c17d04513f54609af2969d5fc6
alexia molina <alexiamolinamanzanedo@gmail.com>
2023-01-19 09:28:30 +0100

    "app icon and login started"

--- app/src/main/java/com/team/seacondlife/LoginActivity.kt
+++ app/src/main/java/com/team/seacondlife/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.team.seacondlife
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        var contra = findViewById<TextView>(R.id.textForgotPassword)
+
+        contra.setOnClickListener(View.OnClickListener { contraToast() })
     }
+
+    fun contraToast() {
+        val toast = Toast.makeText(this, "Sorry, something went wrong", Toast.LENGTH_LONG)
+        toast.show()
+    }
+
 }
