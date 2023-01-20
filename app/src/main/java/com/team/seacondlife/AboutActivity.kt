6e0d2af836bd880a671c0c700526e3c2a48d0316
alexia molina <alexiamolinamanzanedo@gmail.com>
2023-01-20 09:52:29 +0100

    "appbar de about y settings"

--- app/src/main/java/com/team/seacondlife/AboutActivity.kt
+++ app/src/main/java/com/team/seacondlife/AboutActivity.kt
@@ -2,10 +2,20 @@ package com.team.seacondlife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        this.setTitle("Acerca de")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_simple, menu)
+
+        return true
     }
 }
