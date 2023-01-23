0a90270eab1d8dc7e86f9f15f06110007354aadb
Daniel Mora <daniel27mora96@gmail.com>
2023-01-23 12:13:51 +0100

    Created MainFragment from MainActivity

--- app/src/main/java/com/team/seacondlife/fragments/MainFragment.kt
+++ app/src/main/java/com/team/seacondlife/fragments/MainFragment.kt
@@ -1,81 +1,108 @@
-package com.team.seacondlife
+package com.team.seacondlife.fragments
 
 import android.annotation.SuppressLint
 import android.content.ContentValues
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.View
+import android.view.*
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
 import com.jawnnypoo.physicslayout.Physics
+import com.team.seacondlife.R
 import com.team.seacondlife.databinding.ActivityMainBinding
 import org.jbox2d.dynamics.Body
 import org.jbox2d.dynamics.World
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
-    private var index: Int=0
-    private var cont: Int=0
+class MainFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+/*
+    fun onCreateView(
+        inflater: LayoutInflater?,
+        @Nullable container: ViewGroup?,
+        @Nullable savedInstanceState: Bundle?
+    ): View? {
+        val binding: MartianDataBinding = DataBindingUtil.inflate(
+            inflater, R.layout.martian_data, container, false
+        )
+        val view: View = binding.getRoot()
+        //here data must be an instance of the class MarsDataProvider
+        binding.setMarsdata(data)
+        return view
+    }
+*/
+
+    private lateinit var binding: ActivityMainBinding
+    private var index: Int = 0
+    private var cont: Int = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
 
-        this.title = null
+        val view: View = binding.getRoot()
+
+        setHasOptionsMenu(true);
+
+        //this.title = null
 
         binding.sea.physics.isFlingEnabled = true
         binding.sea.physics.isPhysicsEnabled
         binding.sea.physics.isFlingEnabled
 
-        for(i in 0 until binding.sea.childCount){
-            val imageView=binding.sea.getChildAt(i) as ImageView
-            imageView.id=i
+        for (i in 0 until binding.sea.childCount) {
+            val imageView = binding.sea.getChildAt(i) as ImageView
+            imageView.id = i
             imageView.setImageResource(R.drawable.pez)
         }
-        index=binding.sea.childCount+1;
+        index = binding.sea.childCount + 1;
 
         binding.sea.setOnLongClickListener {
             binding.sea.physics.giveRandomImpulse()
             return@setOnLongClickListener true
         }
 
-        binding.sea.physics.setOnCollisionListener(object: Physics.OnCollisionListener{
+        binding.sea.physics.setOnCollisionListener(object : Physics.OnCollisionListener {
             @SuppressLint("SetTextIl8n")
-            override fun onCollisionEntered(viewIdA:Int,viewIdB: Int){
-                Log.d(ContentValues.TAG,"$viewIdB")
-                if(viewIdB!=0) {
+            override fun onCollisionEntered(viewIdA: Int, viewIdB: Int) {
+                Log.d(ContentValues.TAG, "$viewIdB")
+                if (viewIdB != 0) {
                     binding.animal.setImageResource(R.drawable.mantarraya)
-                }else{
+                } else {
                     binding.animal.setImageResource(R.drawable.pezpayaso)
                 }
             }
-            override fun onCollisionExited(viewIdA: Int, viewIdB: Int){}
+
+            override fun onCollisionExited(viewIdA: Int, viewIdB: Int) {}
         })
 
-
-        if(UpdateInDB()) {
-            val pez=ImageView(this)
+        /*
+        if (UpdateInDB()) {
+            val pez = ImageView(this)
             pez.setImageResource(R.drawable.pulpo)
             val layoutParams = ConstraintLayout.LayoutParams(
                 resources.getDimensionPixelSize(R.dimen.square_size),
                 resources.getDimensionPixelSize(R.dimen.square_size)
             )
-            pez.layoutParams=layoutParams
-            pez.id=index
+            pez.layoutParams = layoutParams
+            pez.id = index
             binding.sea.addView(pez)
             index++
         }
+        */
 
-
-        binding.sea.physics.addOnPhysicsProcessedListener(object :Physics.OnPhysicsProcessedListener{
+        binding.sea.physics.addOnPhysicsProcessedListener(object :
+            Physics.OnPhysicsProcessedListener {
             override fun onPhysicsProcessed(physics: Physics, world: World) {
-                Log.d(ContentValues.TAG,"onPhysicsProcessed")
+                Log.d(ContentValues.TAG, "onPhysicsProcessed")
             }
         })
 
@@ -85,37 +112,44 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        Timer().scheduleAtFixedRate(object:TimerTask(){
-            override fun run(){
-                var conf:Boolean=true;
+        Timer().scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                var conf: Boolean = true;
 
-                if(!conf) {
+                if (!conf) {
                     binding.sea.physics.setGravity(
                         ((Math.random() * 9) * (0.1)).toFloat(),
                         ((Math.random() * 9) * (0.1)).toFloat()
                     )
-                    conf=true
-                }else{
+                    conf = true
+                } else {
                     binding.sea.physics.setGravity(
                         ((Math.random() * 9) * (-0.1)).toFloat(),
                         ((Math.random() * 9) * (-0.1)).toFloat()
                     )
-                    conf=false
+                    conf = false
                 }
 
             }
-        },0,10000)
+        }, 0, 10000)
+
+        return view
     }
 
 
     fun UpdateInDB(): Boolean {
         return false
     }
-
+/* mal
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.appbar, menu)
         return true
     }
-}
+*/
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+    }
 
+
+}
