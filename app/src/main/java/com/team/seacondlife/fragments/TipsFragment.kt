394f58a3d3af2c0a84ff557d33e7797ab8bbf631
alexia molina <alexiamolinamanzanedo@gmail.com>
2023-02-08 09:56:35 +0100

    tips almost done

--- app/src/main/java/com/team/seacondlife/fragments/TipsFragment.kt
+++ app/src/main/java/com/team/seacondlife/fragments/TipsFragment.kt
@@ -1,14 +1,20 @@
 package com.team.seacondlife.fragments
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.team.seacondlife.LoginActivity
 import com.team.seacondlife.R
 
 class TipsFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,9 +22,12 @@ class TipsFragment : Fragment() {
     ): View? {
         super.onCreate(savedInstanceState)
         val view: View = inflater.inflate(R.layout.fragment_tips,container,false)
-
-
-
+        var botMas = getView()?.findViewById<Button>(R.id.botonMas)
+        var textInfo = view?.findViewById<TextView>(R.id.info)
+        botMas?.setOnClickListener(View.OnClickListener {
+            textInfo?.setText(R.string.info2.toString())
+        })
         return view
     }
+
 }
