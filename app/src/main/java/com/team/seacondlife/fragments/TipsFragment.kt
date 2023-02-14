6ccd4f4c59f20dd3d288561fd98707e53ec7ccd4
alexia molina <alexiamolinamanzanedo@gmail.com>
2023-02-14 12:36:48 +0100

    tried to fix tips

--- app/src/main/java/com/team/seacondlife/fragments/TipsFragment.kt
+++ app/src/main/java/com/team/seacondlife/fragments/TipsFragment.kt
@@ -1,19 +1,20 @@
 package com.team.seacondlife.fragments
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.team.seacondlife.LoginActivity
 import com.team.seacondlife.R
 
-class TipsFragment : Fragment() {
+
+class TipsFragment : Fragment(), OnClickListener {
+    var textInfo = view?.findViewById<TextView>(com.team.seacondlife.R.id.info)
 
     //hay que arreglar el boton
     override fun onCreateView(
@@ -22,13 +23,15 @@ class TipsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         super.onCreate(savedInstanceState)
-        val view: View = inflater.inflate(R.layout.fragment_tips,container,false)
-        var botMas = getView()?.findViewById<Button>(R.id.botonMas)
-        var textInfo = view?.findViewById<TextView>(R.id.info)
-        botMas?.setOnClickListener(View.OnClickListener {
-            textInfo?.setText(R.string.info2.toString())
-        })
+        val view: View = inflater.inflate(com.team.seacondlife.R.layout.fragment_tips,container,false)
+        var botMas = getView()?.findViewById<Button>(com.team.seacondlife.R.id.botonMas)
+        botMas?.setOnClickListener(this)
         return view
+
+    }
+    override fun onClick(v: View?) {
+        Toast.makeText(this.context, "aaaaaa", Toast.LENGTH_SHORT).show()
+        textInfo?.setText("asdfd")
     }
 
 }
