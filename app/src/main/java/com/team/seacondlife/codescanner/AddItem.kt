6b175c443f79650b6059cf51d5016669940d4265
Daniel Mora <daniel27mora96@gmail.com>
2023-02-28 12:05:10 +0100

    removed badge code

--- app/src/main/java/com/team/seacondlife/codescanner/AddItem.kt
+++ app/src/main/java/com/team/seacondlife/codescanner/AddItem.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.team.seacondlife.R
 
 class AddItem : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
